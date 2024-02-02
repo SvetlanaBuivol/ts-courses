6e1f7847b56ee929c906f82da586d7c55f133e6d
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-02-02 20:35:10 +0200

    +

---
+++ dist/concatination.js
@@ -0,0 +1,6 @@
+function concatination(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+}
+;
+export { concatination };
+//# sourceMappingURL=concatination.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { concatination } from "./concatination";
+
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
+if (button && input) {
+    button.addEventListener('click', () => {
+        concatination(input.value, 'hello!');
+    })
+}
